9ba7983d81569351d87784ef5151fbc5dda9ff02
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-08 20:47:15 +0500

    Added stock data list

--- app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
+++ app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
@@ -2,17 +2,27 @@ package com.gayratrakhimov.reactiveandroidprogramming;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.widget.TextView;
+
+import java.util.Date;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.Observable;
-import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
 
     @BindView(R.id.hello_world_salute)
     TextView helloText;
+
+    @BindView(R.id.stock_updates_recycler_view)
+    RecyclerView recyclerView;
+
+    private LinearLayoutManager layoutManager;
+    private StockDataAdapter stockDataAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,12 +32,22 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         Observable.just("Hello! Please use this app responsibly!")
-                .subscribe(new Consumer<String>() {
-                    @Override
-                    public void accept(String s) throws Exception {
-                        helloText.setText(s);
-                    }
-                });
+                .subscribe(helloText::setText);
+
+        recyclerView.setHasFixedSize(true);
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+        stockDataAdapter = new StockDataAdapter();
+        recyclerView.setAdapter(stockDataAdapter);
+
+        Observable.just(
+                new StockUpdate("GOOGLE", 12.43, new Date()),
+                new StockUpdate("APPL", 645.1, new Date()),
+                new StockUpdate("TWTR", 1.43, new Date())
+        ).subscribe(stockUpdate -> {
+            Log.d("APP", "New update " + stockUpdate.getStockSymbol());
+            stockDataAdapter.add(stockUpdate);
+        });
 
     }
 
