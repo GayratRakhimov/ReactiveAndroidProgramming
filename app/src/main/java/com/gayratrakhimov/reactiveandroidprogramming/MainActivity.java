32f417796008ebb4208c58f9331752eefba4bcbf
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-11 13:12:07 +0500

    Added observable merge example

--- app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
+++ app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.gayratrakhimov.reactiveandroidprogramming.coinapi.CoinApiService;
 import com.gayratrakhimov.reactiveandroidprogramming.coinapi.CoinApiServiceFactory;
@@ -147,12 +146,7 @@ public class MainActivity extends RxAppCompatActivity {
                 .toObservable()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .doOnError(error -> {
-                    log("doOnError", "error");
-                    Toast.makeText(this, "We couldn't reach internet - falling back to local data",
-                            Toast.LENGTH_SHORT)
-                            .show();
-                })
+                .doOnError(ErrorHandler.get())
                 .observeOn(Schedulers.io())
                 .map(ExchangeRatesResult::getRates)
                 .flatMap(Observable::fromIterable)
@@ -183,6 +177,108 @@ public class MainActivity extends RxAppCompatActivity {
                     }
                 });
 
+//        final Configuration configuration = new ConfigurationBuilder()
+//                .setDebugEnabled(BuildConfig.DEBUG)
+//                .setOAuthConsumerKey("pWfKxSWn4Dj0dEhbcouFhwMeN")
+//                .setOAuthConsumerSecret("OXlL55fGXc1hqHhjkkHPYTKAY7Emdmf4DjdziGb82Su8cxxnUf")
+//                .setOAuthAccessToken("195655474-BmqYZkQasiqO5fJu5mK08brpdPmSdpogM1Z24nfw")
+//                .setOAuthAccessTokenSecret("En6ouDbYITOMc7r9lMMh0YvZzq2c9yIpY43YYL0cjHp1Q")
+//                .build();
+//
+//        final FilterQuery filterQuery = new FilterQuery()
+//                .track("Yahoo", "Google", "Microsoft")
+//                .language("en");
+
+//        Observable.merge(
+//                coinApiService.getExchangeRates("USD")
+//                        .toObservable()
+//                        .map(r -> r.getRates())
+//                        .flatMap(Observable::fromIterable)
+//                        .map(StockUpdate::create),
+//                observeTwitterStream(configuration, filterQuery)
+//                        .sample(700, TimeUnit.MILLISECONDS)
+//                        .map(StockUpdate::create)
+//        )
+//                .compose(bindToLifecycle())
+//                .subscribeOn(Schedulers.io())
+//                .doOnError(ErrorHandler.get())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .doOnError(error -> {
+//                    Toast.makeText(this, "We couldn't reach internet - falling back to local data",
+//                            Toast.LENGTH_SHORT)
+//                            .show();
+//                })
+//                .observeOn(Schedulers.io())
+//                .doOnNext(this::saveStockUpdate)
+//                .doOnError(ErrorHandler.get())
+//                .onExceptionResumeNext(
+//                        v2(StorIOFactory.get(this)
+//                                .get()
+//                                .listOfObjects(StockUpdate.class)
+//                                .withQuery(Query.builder()
+//                                        .table(StockUpdateTable.TABLE)
+//                                        .orderBy("date DESC")
+//                                        .limit(50)
+//                                        .build())
+//                                .prepare()
+//                                .asRxObservable())
+//                                .take(1)
+//                                .flatMap(Observable::fromIterable)
+//                )
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(stockUpdate -> {
+//                    Log.d("APP", "New update " + stockUpdate.getStockSymbol());
+//                    noDataAvailableView.setVisibility(View.GONE);
+//                    stockDataAdapter.add(stockUpdate);
+//                    recyclerView.smoothScrollToPosition(0);
+//                }, error -> {
+//                    Log.e("APP", "Error", error);
+//                    if (stockDataAdapter.getItemCount() == 0) {
+//                        noDataAvailableView.setVisibility(View.VISIBLE);
+//                    }
+//                });
+
+    }
+
+    Observable<Status> observeTwitterStream(Configuration configuration, FilterQuery filterQuery) {
+        return Observable.create(emitter -> {
+            final TwitterStream twitterStream = new TwitterStreamFactory(configuration).getInstance();
+
+            emitter.setCancellable(() -> {
+                Schedulers.io().scheduleDirect(() -> twitterStream.cleanUp());
+            });
+
+            StatusListener listener = new StatusListener() {
+                @Override
+                public void onStatus(Status status) {
+                    emitter.onNext(status);
+                }
+
+                @Override
+                public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
+                }
+
+                @Override
+                public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
+                }
+
+                @Override
+                public void onScrubGeo(long userId, long upToStatusId) {
+                }
+
+                @Override
+                public void onStallWarning(StallWarning warning) {
+                }
+
+                @Override
+                public void onException(Exception ex) {
+                    emitter.onError(ex);
+                }
+            };
+
+            twitterStream.addListener(listener);
+            twitterStream.filter(filterQuery);
+        });
     }
 
     private void twitter() {
