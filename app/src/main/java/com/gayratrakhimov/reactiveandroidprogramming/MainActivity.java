b60d8d6aa31a72807443861dc81b05455be5814a
Gayrat Rakhimov <rakhimov.gayrat@gmail.com>
2018-03-14 21:15:12 +0500

    Added RxPreferences

--- app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
+++ app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/MainActivity.java
@@ -1,10 +1,14 @@
 package com.gayratrakhimov.reactiveandroidprogramming;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -302,5 +306,24 @@ public class MainActivity extends RxAppCompatActivity {
     private void log(long value) {
         Log.d("APP", Thread.currentThread().getName() + ":" + value);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
 
---
+++ app/src/main/java/com/gayratrakhimov/reactiveandroidprogramming/SettingsActivity.java
@@ -0,0 +1,27 @@
+package com.gayratrakhimov.reactiveandroidprogramming;
+
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+
+import com.trello.rxlifecycle2.components.support.RxAppCompatActivity;
+
+public class SettingsActivity extends RxAppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getFragmentManager().beginTransaction()
+                .replace(android.R.id.content, new SettingsFragment())
+                .commit();
+
+    }
+
+    public static class SettingsFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.preferences);
+        } }
+
+}
